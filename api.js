c0e54617763f0d33a587f8d185b1e7c0f1e55c2e
ChangxinX <49876282+ChangxinX@users.noreply.github.com>
2019-06-18 13:20:53 -0400

    Update api.js

--- api.js
+++ api.js
@@ -2,12 +2,8 @@
 
 const Api = require('claudia-api-builder');
 const api = new Api();
-const showTables = require('./handlers/showTables');
 const searchTable = require('./handlers/searchTable');
-const test = require('./handlers/test');
 const upload = require('./handlers/upload');
-const upload2 = require('./handlers/upload2');
-const upload1 = require('./handlers/upload1');
 const update = require('./handlers/update');
 
 
@@ -23,15 +19,6 @@ api.put('/update', (request) => {
     return update(request);
 });
 
-api.get('/upload', () => {
-    'use strict'
-    return new api.ApiResponse('<html><head></head><body>\
-     <form method="POST" enctype="multipart/form-data">\
-     <input type="text" name="sampleType" placeholder="Sample Type"><br />\
-      <input type="text" name="dataUploadKey" placeholder="Data upload key"><br />\
-       <input type="file" name="labfile"><br />\
-      <input type="submit"> </form> </body></html>',
-         {'Content-Type': 'text/html'}, 202 );})
 
 
 module.exports = api;
